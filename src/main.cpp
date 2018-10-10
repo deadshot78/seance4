6442683f211affeee0f1b0cc8a556c28b9e0f170
guillaume <gsavreux92@gmail.com>
2018-10-10 10:00:10 +0200

    seance4b

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
+
 #include <vector>
+#include <iostream>
+
+using namespace std;
+
 int main() {
+
+vector <int> tab(3);
+tab[0]=3;
+tab[1]=4;
+tab[2]=5;
+tab.push_back(1000);
+cout << tab[0] << " " << tab[1] << " " << tab[2] << " " << tab[3] << " " <<  endl;
 
 return 0;
 }
